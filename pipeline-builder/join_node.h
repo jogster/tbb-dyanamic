d01426386df85de202369f4d79eeecfe7c490ae4
spectre-ns <dahubley@hotmail.ca>
2021-12-29 21:48:24 -0400

    join node appears to be working will fix bugs as they arise.

--- pipeline-builder/join_node.h
+++ pipeline-builder/join_node.h
@@ -12,13 +12,19 @@ class join_functor
 public:
 	join_functor(std::vector<std::string> inputs)
 	{
-
+		for (auto input : inputs)
+		{
+			tbb::concurrent_queue<std::any> buf;
+			m_buffer.insert({ input, buf});
+		}
 	}
 	template<typename OutputPortsType>
-	void operator()(const std::tuple<std::string, std::any>& in, OutputPortsType& p)
+	void operator()(const std::tuple<std::any>& in, OutputPortsType& p)
 	{
+		auto input_casted = std::any_cast<std::tuple<std::string, std::any>>(std::get<0>(in));
+
 		//save the data to the buffer
-		m_buffer.at(std::get<0>(in)).push(std::get<1>(in));
+		m_buffer.at(std::get<0>(input_casted)).push(std::get<1>(input_casted));
 		
 		//loop through the buffer and check if all our buffers have at least one entry
 		if (std::all_of(std::begin(m_buffer), std::end(m_buffer), [](const auto& buff) {
@@ -34,7 +40,7 @@ public:
 					throw std::exception("join node failed");
 				}
 				
-				output_data.insert({ buffer.first, temp });
+				output_data.insert({buffer.first, temp });
 			}
 			std::get<0>(p).try_put(output_data);
 		}
